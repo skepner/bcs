44df19ca1e60270650005e977569dcb3d712996a
Eugene Skepner <github@skepner.eu>
2016-08-30 18:14:08 +0200

    c

--- bcs.cc
+++ bcs.cc
@@ -19,153 +19,205 @@ size_t decrypt(const char* ciphertext, size_t ciphertext_len, std::string key, c
 
 // ----------------------------------------------------------------------
 
+class Buffer
+{
+      // Bytes
+      // 0-3 - signature BCSD
+      // 4 - version (1)
+      // 5 - algorithm, 1 - EVP_aes_256_cbc
+      // 6 - IV size (4-byte words)
+      // 7 - ciphertext offset (4-byte words)
+      // 8 - IV
+      // [3] -  ciphertext
+
+ private:
+    static constexpr const size_t IV_OFFSET = 8;
+    static constexpr const size_t IV_SIZE = 32;
+    static constexpr const size_t INITIAL_SIZE = 8 + IV_SIZE;
+
+    static constexpr const size_t VERSION_OFFSET = 4;
+    static constexpr const size_t ALGORITHM_OFFSET = 5;
+    static constexpr const size_t IV_SIZE_OFFSET = 6;
+    static constexpr const size_t CIPHERTEXT_OFFSET_OFFSET = 7;
+
+ public:
+    inline Buffer()
+        : data(new unsigned char[INITIAL_SIZE]), reserved(0), used(0)
+        {
+            std::memcpy(data, "BCSD", 4);
+            data[VERSION_OFFSET] = 1;        // version
+            data[ALGORITHM_OFFSET] = 1;        // EVP_aes_256_cbc
+            data[IV_SIZE_OFFSET] = IV_SIZE / 4;
+            data[CIPHERTEXT_OFFSET_OFFSET] = (IV_OFFSET + IV_SIZE) / 4;
+            make_iv();
+        }
+
+    inline ~Buffer()
+        {
+            delete [] data;
+        }
+
+    inline size_t iv_size() const { return data[IV_SIZE_OFFSET] * 4; }
+    inline const unsigned char* iv() const { return data + IV_OFFSET; }
+    inline unsigned char* iv() { return data + IV_OFFSET; }
+
+    inline void dump() const
+        {
+            BIO_dump_fp(stdout, reinterpret_cast<const char*>(data), static_cast<int>(ciphertext_offset() + used));
+        }
+
+    typedef const EVP_CIPHER* (*CIPHER)();
+
+    inline CIPHER algorithm() const
+        {
+            switch (data[ALGORITHM_OFFSET]) {
+              case 1:
+                  return EVP_aes_256_cbc;
+              default:
+                  throw std::runtime_error("Unsupported algorithm id: " + std::to_string(static_cast<unsigned>(data[ALGORITHM_OFFSET])));
+            }
+        }
+
+    inline void reserve(size_t aSize)
+        {
+            if (aSize > reserved) {
+                unsigned char* buf = new unsigned char[ciphertext_offset() + aSize];
+                memcpy(buf, data, ciphertext_offset() + used);
+                delete [] data;
+                data = buf;
+                reserved = aSize;
+            }
+        }
+
+    inline void use_add(int aUse) { used += static_cast<size_t>(aUse); }
+    inline size_t ciphertext_size() const { return used; }
+
+    inline unsigned char* ciphertext() { return data + ciphertext_offset(); }
+    inline const unsigned char* ciphertext() const { return data + ciphertext_offset(); }
+    inline unsigned char* ciphertext_end() { return data + ciphertext_offset() + used; }
+
+ private:
+    unsigned char* data;
+    size_t reserved, used;
+
+    inline size_t ciphertext_offset() const { return data[CIPHERTEXT_OFFSET_OFFSET] * 4; }
+
+    inline void make_iv()
+        {
+            srandomdev();
+            for (size_t i = 0; i < iv_size(); ++i)
+                data[i + IV_OFFSET] = random() & 0xFF;
+        }
+};
+
+// ----------------------------------------------------------------------
+
+class Bsc
+{
+ private:
+    static constexpr const size_t KEY_SIZE = 32;
+
+ public:
+    inline Bsc(std::string aKey)
+        {
+            std::memcpy(mKey, aKey.c_str(), KEY_SIZE);
+            ERR_load_crypto_strings();
+            OpenSSL_add_all_algorithms();
+            OPENSSL_config(nullptr);
+        }
+
+    inline ~Bsc()
+        {
+            EVP_cleanup();
+            ERR_free_strings();
+        }
+
+    inline void encrypt(Buffer& aBuffer, std::string aPlainText)
+        {
+            Context context;
+            if (EVP_EncryptInit_ex(context, aBuffer.algorithm()(), nullptr, key(), aBuffer.iv()) != 1)
+                error("EVP_EncryptInit_ex");
+            aBuffer.reserve(aPlainText.size() * 2);
+            int len;
+            if (EVP_EncryptUpdate(context, aBuffer.ciphertext(), &len, reinterpret_cast<const unsigned char*>(aPlainText.c_str()), static_cast<int>(aPlainText.size())) != 1)
+                error("EVP_EncryptUpdate");
+              // std::cout << "EVP_EncryptUpdate " << len << std::endl;
+            aBuffer.use_add(len);
+            if (EVP_EncryptFinal_ex(context, aBuffer.ciphertext_end(), &len) != 1)
+                error("EVP_EncryptFinal_ex");
+              // std::cout << "EVP_EncryptFinal " << len << std::endl;
+            aBuffer.use_add(len);
+        }
+
+    inline std::string decrypt(const Buffer& aBuffer)
+        {
+            Context context;
+            if (EVP_DecryptInit_ex(context, aBuffer.algorithm()(), nullptr, key(), aBuffer.iv()) != 1)
+                  error("EVP_DecryptInit_ex");
+            std::string plaintext(aBuffer.ciphertext_size() * 2, '#');
+            int len1;
+            if (EVP_DecryptUpdate(context, const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(plaintext.data())), &len1, aBuffer.ciphertext(), static_cast<int>(aBuffer.ciphertext_size())) != 1)
+                  error("EVP_DecryptUpdate");
+              // std::cout << "len1 " << len1 << std::endl;
+            int len2;
+            if (EVP_DecryptFinal_ex(context, const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(plaintext.data() + len1)), &len2) != 1)
+                  error("EVP_DecryptFinal_ex");
+              // std::cout << "len2 " << len2 << std::endl;
+            plaintext.resize(static_cast<size_t>(len1 + len2));
+            return plaintext;
+        }
+
+ private:
+    unsigned char mKey[KEY_SIZE];
+
+    inline const unsigned char* key()
+        {
+            return mKey;
+        }
+
+     [[noreturn]] static inline void error(std::string func)
+        {
+            throw std::runtime_error(func + " failed: " + ERR_error_string(ERR_get_error(), nullptr));
+        }
+
+    class Context
+    {
+     public:
+        inline Context()
+            : context(EVP_CIPHER_CTX_new())
+            {
+                if (!context)
+                    Bsc::error("EVP_CIPHER_CTX_new");
+            }
+
+        inline ~Context()
+            {
+                EVP_CIPHER_CTX_free(context);
+            }
+
+        inline operator EVP_CIPHER_CTX* () { return context; }
+
+     private:
+        EVP_CIPHER_CTX* context;
+    };
+};
+
+// ----------------------------------------------------------------------
+
 int main (void)
 {
-    srandomdev();
     std::string key = "01234567890123456789012345678901";
+    std::string plaintext = "1 The quick brown fox jumps over the lazy dog\n2 The quick brown fox jumps over the lazy dog\n3 The quick brown fox jumps over the lazy dog\n4 The quick brown fox jumps over the lazy dog";
 
-    const char* plaintext = "The quick brown fox jumps over the lazy dog";
+    Bsc bsc(key);
+    Buffer buffer;
+    bsc.encrypt(buffer, plaintext);
+    buffer.dump();
 
-      /* Buffer for ciphertext. Ensure the buffer is long enough for the
-       * ciphertext which may be longer than the plaintext, dependant on the
-       * algorithm and mode
-       */
-    char ciphertext[1024];
-
-      /* Buffer for the decrypted text */
-    char decryptedtext[1024];
-
-    size_t decryptedtext_len, ciphertext_len;
-
-    ERR_load_crypto_strings();
-    OpenSSL_add_all_algorithms();
-    OPENSSL_config(NULL);
-
-      /* Encrypt the plaintext */
-    ciphertext_len = encrypt(plaintext, strlen(plaintext), key, ciphertext);
-
-      /* Do something useful with the ciphertext here */
-    printf("Ciphertext is: %d\n", ciphertext_len);
-    BIO_dump_fp(stdout, ciphertext, static_cast<int>(ciphertext_len));
-
-    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, decryptedtext);
-
-      /* Add a NULL terminator. We are expecting printable text */
-    decryptedtext[decryptedtext_len] = '\0';
-
-      /* Show the decrypted text */
-    printf("Decrypted text is:\n");
-    printf("%s\n", decryptedtext);
-
-    EVP_cleanup();
-    ERR_free_strings();
+    auto decrypted = bsc.decrypt(buffer);
+    std::cout << "decrypted: " << decrypted << std::endl;
 
     return 0;
-}
-
-// ----------------------------------------------------------------------
-
-constexpr const size_t IV_LEN = 32;
-
-void make_iv(unsigned char* buf)
-{
-    for (size_t i = 0; i < IV_LEN; ++i)
-        buf[i] = random() & 0xFF;
-
-} // make_iv
-
-// ----------------------------------------------------------------------
-
-
-size_t encrypt(const char* plaintext, size_t plaintext_len, std::string key, char* ciphertext)
-{
-    EVP_CIPHER_CTX *ctx;
-
-    size_t len;
-
-    size_t ciphertext_len;
-
-      /* Create and initialise the context */
-    if (!(ctx = EVP_CIPHER_CTX_new()))
-        throw std::runtime_error("EVP_CIPHER_CTX_new failed");
-
-    unsigned char key2[32];
-    std::memcpy(key2, key.c_str(), 32);
-    unsigned char iv[IV_LEN];
-    make_iv(iv);
-    memcpy(ciphertext, iv, IV_LEN);
-
-    // const unsigned char* key2 = reinterpret_cast<const unsigned char*>(key.c_str());
-    // const unsigned char* iv2 = reinterpret_cast<const unsigned char*>(iv.c_str());
-
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key2, iv))
-        throw std::runtime_error("EVP_EncryptInit_ex failed");
-
-      /* Provide the message to be encrypted, and obtain the encrypted output.
-       * EVP_EncryptUpdate can be called multiple times if necessary
-       */
-    if(1 != EVP_EncryptUpdate(ctx, const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(ciphertext + IV_LEN)), reinterpret_cast<int*>(&len), reinterpret_cast<const unsigned char*>(plaintext), static_cast<int>(plaintext_len)))
-        throw std::runtime_error("EVP_EncryptUpdate failed");
-    ciphertext_len = IV_LEN + len;
-
-      /* Finalise the encryption. Further ciphertext bytes may be written at
-       * this stage.
-       */
-    if(1 != EVP_EncryptFinal_ex(ctx, reinterpret_cast<unsigned char*>(ciphertext + ciphertext_len), reinterpret_cast<int*>(&len)))
-        throw std::runtime_error("EVP_EncryptFinal_ex failed");
-    ciphertext_len += len;
-
-      /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return ciphertext_len;
-}
-
-// ----------------------------------------------------------------------
-
-size_t decrypt(const char* ciphertext, size_t ciphertext_len, std::string key, char* plaintext)
-{
-    unsigned char iv[IV_LEN];
-    memcpy(iv, ciphertext, IV_LEN);
-
-    EVP_CIPHER_CTX *ctx;
-
-    int len;
-
-      /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        throw std::runtime_error("EVP_CIPHER_CTX_new failed");
-
-      /* Initialise the decryption operation. IMPORTANT - ensure you use a key
-       * and IV size appropriate for your cipher
-       * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-       * IV size for *most* modes is the same as the block size. For AES this
-       * is 128 bits */
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, reinterpret_cast<const unsigned char*>(key.c_str()), iv))
-        throw std::runtime_error("EVP_DecryptInit_ex failed");
-    std::cerr << "EVP_DecryptInit_ex" << std::endl;
-
-      /* Provide the message to be decrypted, and obtain the plaintext output.
-       * EVP_DecryptUpdate can be called multiple times if necessary
-       */
-    if(1 != EVP_DecryptUpdate(ctx, const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(plaintext)), &len, reinterpret_cast<const unsigned char*>(ciphertext + IV_LEN), static_cast<int>(ciphertext_len - IV_LEN)))
-        throw std::runtime_error("EVP_DecryptUpdate failed");
-    std::cerr << "EVP_DecryptUpdate " << len << std::endl;
-    size_t plaintext_len = len;
-
-      /* Finalise the decryption. Further plaintext bytes may be written at
-       * this stage.
-       */
-    if(1 != EVP_DecryptFinal_ex(ctx, reinterpret_cast<unsigned char*>(plaintext + plaintext_len), &len))
-        std::cerr << "EVP_DecryptFinal_ex failed: " << ERR_error_string(ERR_get_error(), nullptr) << std::endl;
-        // throw std::runtime_error("EVP_DecryptFinal_ex failed");
-    else
-        plaintext_len += len;
-
-      /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
 }
 
 // ----------------------------------------------------------------------
