98cbc2f4a6c3961354f834b9cc000149601ecdaa
Eugene Skepner <github@skepner.eu>
2016-10-26 10:32:30 +0200

    encrypted sample support added

--- bcs.cc
+++ bcs.cc
@@ -10,7 +10,7 @@
 // ----------------------------------------------------------------------
 
 [[noreturn]] static void usage(const char* progname);
-std::string get_password(bool repeat);
+std::string get_password(bool repeat, std::string encrypted_sample);
 
 // ----------------------------------------------------------------------
 
@@ -25,16 +25,18 @@ int main(int argc, char* const* argv)
     int exit_code = 0;
     ClientCommand command = Decrypt;
     bool via_server = false;
+    std::string encrypted_sample;
     option longopts[] = {
         {"encrypt",     no_argument, nullptr, 'e'},
         {"decrypt",     no_argument, nullptr, 'd'},
         {"server",      no_argument, nullptr, 's'},
         {"client",      no_argument, nullptr, 'c'},
         {"kill-server", no_argument, nullptr, 'k'},
+        {"encrypted-sample", required_argument, nullptr, 'z'},
         { nullptr,      0,           nullptr, 0}
     };
     int ch;
-    while ((ch = getopt_long(argc, argv, "edsck", longopts, nullptr)) != -1) {
+    while ((ch = getopt_long(argc, argv, "edsckz:", longopts, nullptr)) != -1) {
         switch (ch) {
           case 'e':
               command = Encrypt;
@@ -51,6 +53,9 @@ int main(int argc, char* const* argv)
           case 'k':
               command = StopServer;
               break;
+          case 'z':
+              encrypted_sample = optarg;
+              break;
           default:
               usage(argv[0]);
         }
@@ -66,7 +71,7 @@ int main(int argc, char* const* argv)
               if (via_server)
                   client_command(socket_path, true, argv[0], argv[1]);
               else
-                  write_file(argv[1], encrypt(get_password(true), read_file(argv[0])));
+                  write_file(argv[1], encrypt(get_password(true, encrypted_sample), read_file(argv[0])));
               break;
           case Decrypt:
               if (argc != 2)
@@ -74,10 +79,10 @@ int main(int argc, char* const* argv)
               if (via_server)
                   client_command(socket_path, false, argv[0], argv[1]);
               else
-                  write_file(argv[1], decrypt(get_password(false), read_file(argv[0])));
+                  write_file(argv[1], decrypt(get_password(false, encrypted_sample), read_file(argv[0])));
               break;
           case Server:
-              server(socket_path, get_password(true));
+              server(socket_path, get_password(true, encrypted_sample));
               break;
           case StopServer:
               client(socket_path, KillServer);
@@ -94,12 +99,21 @@ int main(int argc, char* const* argv)
 
 // ----------------------------------------------------------------------
 
-std::string get_password(bool repeat)
+std::string get_password(bool repeat, std::string encrypted_sample)
 {
     std::string passwd = getpass("Password: ");
     if (repeat) {
         if (passwd != getpass("Repeat: "))
             throw std::runtime_error("Password mismatch");
+    }
+    if (!encrypted_sample.empty()) {
+          // check password by decrypting sample
+        try {
+            decrypt(passwd, read_file(encrypted_sample));
+        }
+        catch (std::exception& err) {
+            throw std::runtime_error(std::string("Cannot decrypt sample with the entered password: ") + err.what());
+        }
     }
     return passwd;
 
@@ -109,7 +123,7 @@ std::string get_password(bool repeat)
 
 void usage(const char* progname)
 {
-    std::cerr << "Usage: " << progname << "[-e|--encrypt] [-d|--decrypt] <input> <output>" << std::endl;
+    std::cerr << "Usage: " << progname << " [-e|--encrypt] [-d|--decrypt] [-s|--server] [-c|--client] [-k|--kill-server] [<input> <output>]" << std::endl;
     exit(1);
 }
 
